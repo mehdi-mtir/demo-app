c9b3601a7da39762ea205220e560dbf2704178b6
mehdi-mtir <mehdi.mtir@gmail.com>
2020-12-06 16:15:40 +0100

    Demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-app';
+  title = 'Bienvenu dans notre première application!*******';
+  logo = "assets/logoBlack.png";
+  jours = ["Lundi", "Mardi", "Mercredi", "..."]
+
+
+  showMessage(){
+    return ("Message de test");
+  }
+
+  joursFiltered(){
+    return this.jours.filter(
+      value => value != "Lundi"
+    );
+  }
+
+  changeImage(){
+    if (this.logo == "assets/logoBlack.png"){
+      this.logo = "assets/angular.png";
+    }
+    else{
+      this.logo = "assets/logoBlack.png";
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
+  declarations: [ //tous les composants de ce module
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
